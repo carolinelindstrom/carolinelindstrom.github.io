0ba0f8c208cc214f4ccb3278cfa6cc287046e8f8
Erik Markström <erimar@kth.se>
2016-03-17 20:12:51 +0100

    google länkar hype

--- js/classes.js
+++ js/classes.js
@@ -236,7 +236,7 @@ class Donut {
           .attr("x", (screenWidth / 2) + 160*widthFactor)
           .attr('dy', 25)
           .attr('id', 'text-span')
-          .text("More info: http://google.com/#q=" + searchWord);
+          .text("Try this: http://google.com/#q=" + searchWord);
       }
 
     }
@@ -250,7 +250,15 @@ class Donut {
       .append("tspan")
       .attr("x", (screenWidth / 2) + 160*widthFactor)
       .attr('id', 'text-span')
-      .text("Unfortunately we don't have any additional data about "+ data.key +".");
+      .text("Unfortunately we don't have any additional data about "+ data.key +".")
+      .append("a")
+      .attr("id", "google-link")
+      .on("click", function(){ d3.select(this).attr("target", "_blank").attr("xlink:href", 'http://google.com/#q=' + searchWord);})
+      .append("tspan")
+      .attr("x", (screenWidth / 2) + 160*widthFactor)
+      .attr('dy', 25)
+      .attr('id', 'text-span')
+      .text("Try this: http://google.com/#q=" + searchWord);
 
 
     WIKIPEDIA.getData("http://en.wikipedia.org/wiki/" + searchWord, display, function(error) {
@@ -261,7 +269,15 @@ class Donut {
           .classed("active-section", true)
           .classed("hidden-section", false)
           .attr('id', 'text-span')
-          .text("Sorry, there was an issue getting data for "+data.key+".");
+          .text("Sorry, there was an issue getting data for "+data.key+".")
+          .append("a")
+          .attr("id", "google-link")
+          .on("click", function(){ d3.select(this).attr("target", "_blank").attr("xlink:href", 'http://google.com/#q=' + searchWord);})
+          .append("tspan")
+          .attr("x", (screenWidth / 2) + 160*widthFactor)
+          .attr('dy', 25)
+          .attr('id', 'text-span')
+          .text("Try this: http://google.com/#q=" + searchWord);
       }
     });
   }
